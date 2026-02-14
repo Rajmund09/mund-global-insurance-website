ced223ff1664a52e34a99c1efc02afc25fbee4af
Rajmund09 <prabhushankarmund@gmail.com>
2026-02-14 18:18:28 +0530

    Update script.js

--- script.js
+++ script.js
@@ -12,7 +12,6 @@ document.addEventListener("DOMContentLoaded", function () {
     // Mobile menu toggle
     const hamburger = document.getElementById('hamburger');
     const navMenu = document.getElementById('nav-menu');
-
     hamburger.addEventListener('click', () => {
         hamburger.classList.toggle('active');
         navMenu.classList.toggle('active');
